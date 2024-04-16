e6540b45ed7d4afdbf42302a5e92d6bb268ea901
Jemma-MS <jemma.m_s@outlook.com>
2024-04-15 21:39:07 -0600

    first commit

---
+++ pokeapp/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  pokeapp
+//
+//  Created by Jemma Marin on 15/04/2024.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let baseURL = "https://pokedex-bb36f.firebaseio.com/pokemon.json"
+}
+
---
+++ pokeapp/pokeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeappApp.swift
+//  pokeapp
+//
+//  Created by Jemma Marin on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct pokeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
